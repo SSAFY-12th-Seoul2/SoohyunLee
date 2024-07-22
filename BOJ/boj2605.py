f2662c2ae83af52d38b91c282d9583ee8d5a224b
suhyun lee <1eesuhyun521@gmail.com>
2024-07-22 14:21:44 +0900

    [solved] boj2605

---
+++ BOJ/boj2605.py
@@ -0,0 +1,9 @@
+T = int(input())
+lst = list(map(int, input().split()))
+new_lst = []
+for ind, value in enumerate(lst):
+    new_ind = ind-value
+    new_lst.insert(new_ind, ind+1)
+
+for num in new_lst:
+    print(num, end=" ")
